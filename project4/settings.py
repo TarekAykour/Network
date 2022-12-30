96f9fbc36c38af781938ea333a4ba66700ce23e4
TarekAykour <73133367+TarekAykour@users.noreply.github.com>
2022-12-30 17:18:15 +0100

    github overwrote files

--- project4/settings.py
+++ project4/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
+    'fontawesome_5'
     
 ]
 
@@ -122,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+MEDIA_URL = '/media/'
